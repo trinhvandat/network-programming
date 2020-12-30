e749716ed8d80cf27c08dcac7ca8b9105ef0f2e1
leonard <trinhvandat90399@gmail.com>
2020-12-30 12:03:27 +0700

    Create client-server send-receive bytes array

--- tcp-demo/src/com/ptit/tcp/inputstream/ByteStreamClient.java
+++ tcp-demo/src/com/ptit/tcp/inputstream/ByteStreamClient.java
@@ -1,4 +1,54 @@
 package com.ptit.tcp.inputstream;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 public class ByteStreamClient {
+
+    private final static Logger logger = Logger.getLogger(String.valueOf(ByteStreamClient.class));
+
+    private static final int PORT = 1108;
+    private static final String HOST = "localhost";
+
+    public static void main(String[] args) {
+        try{
+            Socket socket = new Socket(HOST, PORT);
+
+            InputStream inputStream = socket.getInputStream();
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+            OutputStream outputStream = socket.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+            String studentInfo = "B17DCCN123;1108";
+            dataOutputStream.writeUTF(studentInfo);
+
+            String myExam = "";
+            logger.info("exam: " + myExam);
+            int a = 0;
+            while ((a = dataInputStream.read()) != -1){
+                char h = (char) a;
+                myExam += h;
+            }
+            logger.info("exam: " + myExam);
+
+            String[] myExamStringsArray = myExam.split(";");
+            List<Integer> myExamDoubleList = new ArrayList<>();
+            for (String exam : myExamStringsArray){
+                myExamDoubleList.add(Integer.valueOf(exam));
+            }
+            logger.info("double list: " + myExamDoubleList.toString());
+        } catch (UnknownHostException e) {
+            logger.warning("unknown host tcp server");
+            e.printStackTrace();
+        } catch (IOException e) {
+            logger.warning("I/O error: please check instance socket at line: 17");
+            e.printStackTrace();
+        }
+    }
+
 }
--- tcp-demo/src/com/ptit/tcp/inputstream/ByteStreamServer.java
+++ tcp-demo/src/com/ptit/tcp/inputstream/ByteStreamServer.java
@@ -1,7 +1,8 @@
 package com.ptit.tcp.inputstream;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.logging.Logger;
 
 public class ByteStreamServer {
@@ -16,11 +17,53 @@ public class ByteStreamServer {
             ServerSocket serverSocket = new ServerSocket(PORT);
             logger.info("listening to port:" + PORT);
 
+            while(true){
+                Socket socket = serverSocket.accept();
+                logger.info("connecting with client: " + socket.getInetAddress() + ":" + socket.getPort());
+
+                InputStream inputStream = socket.getInputStream();
+                DataInputStream dataInputStream;
+
+                OutputStream outputStream = socket.getOutputStream();
+                DataOutputStream dataOutputStream;
+
+                dataInputStream = new DataInputStream(inputStream);
+                String studentInfoReceived = dataInputStream.readUTF();
+                String[] studentInfoStringArrays = studentInfoReceived.split(";");
+                logger.info("student info: " + studentInfoStringArrays[0].toLowerCase());
+
+                dataOutputStream = new DataOutputStream(outputStream);
+                if (studentInfoStringArrays[0].toLowerCase().equals("b17dccn123")) {
+                    logger.info("Student's information exactly!");
+                    String exam = "";
+                    for (int index = 0; index <= 10; index ++){
+                        exam += Math.random() + " ";
+                    }
+                    exam += Math.random();
+                    exam = "1;2;3;4;5";
+                    logger.info("exam for student: " + exam);
+                    byte[] examBytesArray = exam.getBytes();
+                    logger.info("all byte count: " + examBytesArray.length);
+                    dataOutputStream.write(examBytesArray);
+                    logger.info("send exam to student with student's code: " + studentInfoStringArrays[0].toLowerCase() + " successful.");
+                    dataOutputStream.close();
+                }
+                else {
+                    logger.warning("Incorrect student's information");
+                    String returnResultVerifyStudentInfo = "Incorrect student's information. Please check your student's information !!!";
+                    byte[] returnResultVerifyStudentInfoBytesArray = returnResultVerifyStudentInfo.getBytes();
+                    dataOutputStream.write(returnResultVerifyStudentInfoBytesArray);
+                    dataOutputStream.close();
+                }
+
+
+
+            }
+
         } catch (IOException e) {
             logger.warning("I/O error: " + e);
             e.printStackTrace();
         }
-
     }
 
 }
